9ac099de16841d5b834d6d86500657b9318dc58d
Adam Laverack <adalator@googlemail.com>
2018-09-12 11:38:25 +0100

    Added ability to choose type of nuclear-quantum-enabled calculation

---
+++ pymuonsuite/nuclearquantum/__main__.py
@@ -0,0 +1,25 @@
+# Python 2-to-3 compatibility code
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import argparse as ap
+
+from pymuonsuite.nuclearquantum.phonon import phonon_hfcc
+
+if __name__ == "__main__":
+    parser = ap.ArgumentParser()
+    parser.add_argument('calculation_type', type=str,
+        help="""Type of calculation to be performed, currently supports:
+                Quantum effects as phonons: phonon_hfcc""")
+    parser.add_argument('parameter_file', type=str,
+        help="YAML file containing relevant input parameters")
+
+    args = parser.parse_args()
+
+    if args.calculation_type == "phonon_hfcc":
+        phonon_hfcc(args.parameter_file)
+    else:
+        raise RuntimeError("""Invalid calculation type entered, please use
+                              python -h flag to see currently supported types""")
---
+++ pymuonsuite/nuclearquantum/phonon.py
@@ -0,0 +1,9 @@
+# Python 2-to-3 compatibility code
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+def phonon_hfcc(param_file):
+    print(param_file)
+    return
