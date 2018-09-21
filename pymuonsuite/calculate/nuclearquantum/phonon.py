4d37dbedd922cee3da122877e65e658b96e98515
Adam Laverack <adalator@googlemail.com>
2018-09-21 16:16:18 +0100

    Broke phonon_average() up into 4 functions

--- pymuonsuite/calculate/nuclearquantum/phonon.py
+++ pymuonsuite/calculate/nuclearquantum/phonon.py
@@ -74,8 +74,8 @@ def get_major_emodes(evecs, i):
 
     return major_evecs_i, major_evecs, major_evecs_ortho
 
-def phonon_average(R, grid_n, sname, symbols, freqs, E_table, hfine_table, hfine_tensors, all_hfine_tensors, mu_mass,
-                    ipso_hfine_table = None, ipso_hfine_tensors = None, num_solve = False, noH = False, save_tens = False):
+def calc_wavefunction(R, grid_n, mu_mass, E_table, hfine_table, sname = None,
+                        num_solve = False, write_table = True):
     R_axes = np.array([np.linspace(-3*Ri, 3*Ri, grid_n)
                        for Ri in R])
 
@@ -91,21 +91,26 @@ def phonon_average(R, grid_n, sname, symbols, freqs, E_table, hfine_table, hfine
         except ImportError:
             raise RuntimeError('QLab not present on this system, '
                                '-num option is invalid')
-        sname += '_num'
+        if write_table:
+            sname += '_num'
         psi = []
         for i, Ri in enumerate(R):
-            qSol = QSolution([(-3e-10*Ri, 3e-10*Ri)], params['grid_n'],
-                             E_table[i]*cnst.electron_volt, params['m'])
+            qSol = QSolution([(-3e-10*Ri, 3e-10*Ri)], grid_n,
+                             E_table[i]*cnst.electron_volt, mu_mass)
             psi.append(qSol.evec_grid(0))
         psi = np.array(psi)
-        # Oh, and save the densities!
-
-    psi_table = np.concatenate(
-        (R_axes, E_table, psi**2, hfine_table), axis=0)
-    np.savetxt(sname + '_psi.dat', psi_table.T)
+    # Oh, and save the densities!
+    if write_table:
+        psi_table = np.concatenate(
+            (R_axes, E_table, psi**2, hfine_table), axis=0)
+        np.savetxt(sname + '_psi.dat', psi_table.T)
     # And average
     r2psi2 = R_axes**2*np.abs(psi)**2
 
+    return r2psi2
+
+def avg_hfine_tensor(r2psi2, hfine_table, hfine_tensors, noH, ipso_hfine_table = None,
+                        ipso_hfine_tensors = None, sname = None):
     hfine_avg = np.sum(r2psi2*hfine_table)/np.sum(r2psi2)
     # Now average of dipolar components
     hfine_tens_avg = np.sum(
@@ -148,18 +153,23 @@ def phonon_average(R, grid_n, sname, symbols, freqs, E_table, hfine_table, hfine
         ipso_D1 = None
         ipso_D2 = None
         np.savetxt(sname + '_tensors.dat', hfine_tens_avg)
+        
+    return D1, D2, ipso_D1, ipso_D2
 
-    if save_tens:
-        # Also save tensor file
-        tensfile = open(sname + '_tensors.dat', 'w')
-        for i in range(np.size(all_hfine_tensors, 0)):
-            hfine_tensors_i = all_hfine_tensors[i]
-            # Carry out the average
-            hfine_avg = np.sum(
-            r2psi2[:, :, None, None]*hfine_tensors_i, axis=(0, 1))/np.sum(r2psi2)
-            tensfile.write('{0} {1}\n'.format(symbols[i], i))
-            tensfile.write('\n'.join(['\t'.join([str(x) for x in l]) for l in hfine_avg]) + '\n')
+def write_tensors(sname, all_hfine_tensors, r2psi2, symbols):
+    # Also save tensor file
+    tensfile = open(sname + '_tensors.dat', 'w')
+    for i in range(np.size(all_hfine_tensors, 0)):
+        hfine_tensors_i = all_hfine_tensors[i]
+        # Carry out the average
+        hfine_avg = np.sum(
+        r2psi2[:, :, None, None]*hfine_tensors_i, axis=(0, 1))/np.sum(r2psi2)
+        tensfile.write('{0} {1}\n'.format(symbols[i], i))
+        tensfile.write('\n'.join(['\t'.join([str(x) for x in l]) for l in hfine_avg]) + '\n')
 
+def calc_harm_potential(R, grid_n, mu_mass, freqs, E_table, sname):
+    R_axes = np.array([np.linspace(-3*Ri, 3*Ri, grid_n)
+                       for Ri in R])
     # Now the potential, measured vs. theoretical
     harm_K = mu_mass*freqs**2
     harm_V = (0.5*harm_K[:, None]*(R_axes*1e-10)**2)/cnst.electron_volt
@@ -171,9 +181,6 @@ def phonon_average(R, grid_n, sname, symbols, freqs, E_table, hfine_table, hfine
                     E_table[:, E_table.shape[1]//2-1])[:, None]/2.0
     all_table = np.concatenate((R_axes, harm_V, E_table), axis=0)
     np.savetxt(sname + '_V.dat', all_table.T)
-
-    return D1, D2, ipso_D1, ipso_D2
-
 
 def phonon_hfcc(param_file):
     #Load parameters
@@ -255,6 +262,8 @@ def phonon_hfcc(param_file):
                 hfine_table[i][j] = np.trace(mgr.get_array('hyperfine')[mu_index])/3.0
                 if not params['ignore_ipsoH']:
                     ipso_hfine_table[i][j] = np.trace(mgr.get_array('hyperfine')[ipso_H_index])/3.0
+                else:
+                    ipso_hfine_table = None
                 for k, tensor in enumerate(mgr.get_array('hyperfine')):
                     all_hfine_tensors[k][i][j][:][:] = tensor
                 castf = os.path.join(
@@ -266,11 +275,13 @@ def phonon_hfcc(param_file):
             raise RuntimeError("Incomplete or absent magres or castep data")
 
         symbols = cell.get_array('castep_custom_species')
-        D1, D2, ipso_D1, ipso_D2 = phonon_average(R, params['grid_n'], sname, symbols, evals, E_table, hfine_table, all_hfine_tensors[mu_index],
-                            all_hfine_tensors, mu_mass, ipso_hfine_table, all_hfine_tensors[ipso_H_index], num_solve = params['numerical_solver'],
-                            noH = params['ignore_ipsoH'], save_tens = params['save_tensors'])
 
-        print(D1, D2, ipso_D1, ipso_D2)
+        r2psi2 = calc_wavefunction(R, params['grid_n'], mu_mass, E_table, hfine_table, sname,
+                                False, True)
+        D1, D2, ipso_D1, ipso_D2 = avg_hfine_tensor(r2psi2, hfine_table, all_hfine_tensors[mu_index],
+                                                params['ignore_ipsoH'], ipso_hfine_table, all_hfine_tensors[ipso_H_index], sname)
+        write_tensors(sname, all_hfine_tensors, r2psi2, symbols)
+        calc_harm_potential(R, params['grid_n'], mu_mass, evals, E_table, sname)
 
     return
 
