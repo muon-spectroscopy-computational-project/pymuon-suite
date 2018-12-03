3b76ba19c9f417378f3201656fb9da5e09f4e7b1
Adam Laverack <adalator@googlemail.com>
2018-12-03 15:54:47 +0000

    Implmented hyperfine constant report writer

--- pymuonsuite/quantum/vibrational/programs.py
+++ pymuonsuite/quantum/vibrational/programs.py
@@ -37,6 +37,33 @@ SSH:    git@bitbucket.org:casteppy/casteppy.git
 
 and try again.""")
 
+def hfine_report(R, grid_n, tensors, hfine_tens_avg, r2psi2, sname, symbol):
+    hfine_table = np.zeros((np.size(R), grid_n))
+    for i, axis in enumerate(tensors):
+        for j, tensor in enumerate(axis):
+            hfine_table[i][j] = np.trace(tensor)/3.0
+
+    ofile = open(sname + '_report.txt', 'a')
+
+    hfine_avg = np.sum(r2psi2*hfine_table)/np.sum(r2psi2)
+    ofile.write('Predicted hyperfine coupling on labeled atom ({1}): {0} MHz\n'.format(
+        hfine_avg, symbol))
+
+    evals, evecs = np.linalg.eigh(hfine_tens_avg)
+    evals, evecs = zip(*sorted(zip(evals, evecs), key=lambda x: abs(x[0])))
+    evals_notr = -np.array(evals)+np.average(evals)
+
+    if abs(evals_notr[2]) > abs(evals_notr[0]):
+        D1 = evals_notr[2]
+        D2 = evals_notr[1]-evals_notr[0]
+    else:
+        D1 = evals_notr[0]
+        D2 = evals_notr[2]-evals_notr[1]
+
+    ofile.write(('Predicted dipolar hyperfine components on labeled atom ({2}):\n'
+                 'D1:\t{0} MHz\nD2:\t{1} MHz\n').format(
+        D1, D2, symbol))
+
 def muon_harmonic(cell_f, mu_sym, grid_n, property, calc='castep', pname=None,
                 ignore_ipsoH=False, solver=False, args_w=False,
                 ase_phonons=False, dftb_phonons=False):
@@ -74,7 +101,7 @@ def muon_harmonic(cell_f, mu_sym, grid_n, property, calc='castep', pname=None,
     sname = seedname(cell_f)
     #Parse muon data using appropriate parser for calculator
     if (calc.strip().lower() in 'castep'):
-        mu_index, ipso_H_index, mu_mass = parse_castep_muon(cell, mu_sym,
+        mu_index, iH_index, mu_mass = parse_castep_muon(cell, mu_sym,
                                                             ignore_ipsoH)
     else:
         raise RuntimeError("Invalid calculator entered ('{0}').".format(calc))
@@ -132,13 +159,6 @@ def muon_harmonic(cell_f, mu_sym, grid_n, property, calc='castep', pname=None,
                     tensor_file = os.path.join(dirname,
                         '{0}_{1}_{2}/{0}_{1}_{2}.magres'.format(sname, i+1, j))
                     tensors = (parse_hyperfine_magres(tensor_file)).get_array('hyperfine')
-                    hfine_table[i][j] = np.trace(
-                        tensors[mu_index])/3.0
-                    if not ignore_ipsoH:
-                        ipso_hfine_table[i][j] = np.trace(
-                            tensors[ipso_H_index])/3.0
-                    else:
-                        ipso_hfine_table = None
                 else:
                     raise ValueError("Invalid value for property")
                 for k, tensor in enumerate(tensors):
@@ -155,10 +175,15 @@ def muon_harmonic(cell_f, mu_sym, grid_n, property, calc='castep', pname=None,
 
         r2psi2 = calc_wavefunction(R, grid_n, E_table = E_table,
             write_table = True, value_table = hfine_table, sname = sname)
-
         tens_avg = weighted_tens_avg(grid_tensors, r2psi2)
         write_tensors(tens_avg, sname, symbols)
-
         calc_harm_potential(R, grid_n, mu_mass, mu_evals, E_table, sname)
+
+        if property == 'hyperfine':
+            hfine_report(R, grid_n, grid_tensors[mu_index], tens_avg[mu_index],
+                r2psi2, sname, mu_sym)
+            if not ignore_ipsoH:
+                hfine_report(R, grid_n, grid_tensors[iH_index], tens_avg[iH_index],
+                r2psi2, sname, "{0} {1} (ipso)".format(symbols[iH_index], iH_index))
 
     return
