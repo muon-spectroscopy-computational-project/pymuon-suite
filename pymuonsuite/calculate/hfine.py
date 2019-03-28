6ac5ec28257bbc185807573a056b4cb295f22b0b
stur86 <simonesturniolo@gmail.com>
2019-03-28 18:54:26 +0000

    Added computation of hyperfine tensor (to be tested)

--- pymuonsuite/calculate/hfine.py
+++ pymuonsuite/calculate/hfine.py
@@ -10,8 +10,13 @@ import scipy.constants as cnst
 from soprano.utils import minimum_supcell, supcell_gridgen
 from soprano.properties.nmr.utils import _get_isotope_data
 
+from pymuonsuite import constants as mu_cnst
 
-def compute_hfine_tensor(points, spins, cell=None, i=0, species='e'):
+_bohrmag = cnst.physical_constants['Bohr magneton'][0]
+
+
+def compute_hfine_tensor(points, spins, cell=None, self_i=0, species='e',
+                         cut_r=10):
     """Compute the hyperfine tensor experienced at point of index i generated
     by a number of localised spins at points, for a given periodic unit cell
     and species.
@@ -22,15 +27,67 @@ def compute_hfine_tensor(points, spins, cell=None, i=0, species='e'):
     |   spins (np.ndarray):  magnetic moments (in Bohr magnetons for electrons
     |                        or hbar*gamma units for nuclei)
     |   cell (np.ndarray):   unit cell (if None, considered non-periodic)
-    |   i (int):             index of point at which to compute the tensor.
+    |   self_i (int):        index of point at which to compute the tensor.
     |                        Local spin density will give rise to a Fermi 
     |                        contact term
     |   species (str or [str]): symbol or list of symbols identifying the
     |                           species generating the magnetic field.
     |                           Determines the magnetic moments
+    |   cut_r (float):       cutoff radius for dipolar component calculation
 
     | Returns:
     |   HT (np.ndarray):    hyperfine tensor at point i
     """
 
-    pass
+    N = len(points)
+
+    magmoms = np.array(spins).astype(float)
+    species = np.array(species).astype('S2')
+    if species.shape == ():
+        species = np.repeat(species[None], N)
+    for i, s in enumerate(species):
+        if s == 'e':
+            mm = 2*_bohrmag
+        elif s == 'mu':
+            mm = mu_cnst.m_gamma*cnst.hbar
+        else:
+            mm = _get_isotope_data(s, 'gamma')[0]*cnst.hbar
+        magmoms[i] *= mm
+
+    # Do we need a supercell?
+    r = np.array(points)-points[self_i]
+    if cell is not None:
+        scell = minimum_supcell(cut_r, latt_cart=cell)
+        fxyz, xyz = supcell_gridgen(cell, scell)
+        r = r[:, None, :] + xyz[None, :, :]
+    else:
+        r = r[:, None, :]
+
+    rnorm = np.linalg.norm(r, axis=-1)
+    # Expunge the ones that are outside of the sphere
+    sphere = np.where(rnorm <= cut_r)
+    r = r[sphere]
+    rnorm = rnorm[sphere]
+    magmoms = magmoms[sphere[0]]
+
+    # Find the contact point
+    self_i = np.argmin(rnorm)
+    fermi_mm = magmoms[self_i]
+    magmoms[self_i] = 0
+
+    rnorm_inv = 1.0/np.where(rnorm > 0, rnorm, np.inf)
+    rdyad = r[:, None, :]*r[:, :, None]
+    rdip = 3*rdyad*rnorm_inv[:, None, None]**2-np.eye(3)[None, :, :]
+
+    HT = np.sum(magmoms[:, None, None]*rdip *
+                rnorm_inv[:, None, None]**3, axis=0)
+    HT *= cnst.mu_0/(4*np.pi)*1e30
+
+    # Add Lorentz term
+    if cell is not None:
+        avgM = np.sum(magmoms)*3.0/(4.0*np.pi*cut_r**3)
+        HT += np.eye(3)*avgM*cnst.mu_0/3.0*1e30
+    # Add contact term
+    HT += np.eye(3)*fermi_mm*2.0/3.0*cnst.mu_0
+
+    return HT
