Fixed sign problem with muon gyromagnetic ratio

pymuonsuite/constants.py
@@ -8,4 +8,5 @@ import scipy.constants as cnst
 
 m_mu = cnst.physical_constants['muon mass'][0]
 m_mu_amu = cnst.physical_constants['muon mass in u'][0]
-m_gamma = cnst.physical_constants['muon mag. mom.'][0]*2/cnst.hbar
+# Made positive because we need positive muons
+m_gamma = abs(cnst.physical_constants['muon mag. mom.'][0]*2/cnst.hbar)