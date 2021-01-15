144fbfa04c64c3fee986a90a46845160deee0492
Laura Murgatroyd <laura.murgatroyd@stfc.ac.uk>
2021-01-15 11:28:35 +0000

    Add muon nuclear magnetic moment to constants

--- pymuonsuite/constants.py
+++ pymuonsuite/constants.py
@@ -10,3 +10,5 @@ m_mu = cnst.physical_constants['muon mass'][0]
 m_mu_amu = cnst.physical_constants['muon mass in u'][0]
 # Made positive because we need positive muons
 m_gamma = abs(cnst.physical_constants['muon mag. mom.'][0]*2/cnst.hbar)
+mu_nmagm = abs(
+    cnst.physical_constants['muon magn. moment to nuclear magneton ratio'][0])
