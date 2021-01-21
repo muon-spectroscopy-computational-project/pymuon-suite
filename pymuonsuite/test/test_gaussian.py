8565bb37248b142f73f31e28e4e923899037d5b2
Laura Murgatroyd <laura.murgatroyd@stfc.ac.uk>
2021-01-21 16:32:18 +0000

    Add test for writing cell

--- pymuonsuite/test/test_gaussian.py
+++ pymuonsuite/test/test_gaussian.py
@@ -25,6 +25,8 @@ _positions = [[0.00000000,       0.00000000,       0.66748000],
               [0.00000000,       0.92283200,      -1.23769500],
               [0.00000000,      -0.92283200,      -1.23769500]]
 
+_cell = [[10,0,0],[0,10,0],[0,0,10]]
+
 
 class TestReadWriteGaussian(unittest.TestCase):
 
@@ -39,60 +41,63 @@ class TestReadWriteGaussian(unittest.TestCase):
         assert np.allclose(atoms_read.cell, atoms.cell)
 
     def test_write(self):
-        os.chdir(_TESTDATA_DIR)
-        out_folder = "test_gaussian"
-        os.mkdir(out_folder)
-        sname = out_folder
-        atoms = Atoms('C2H4', positions=_positions)
-        muon = Atom('H', position=[1.00000000, 0.00000000,
-                                   0.66748000])
-        atoms = atoms + muon
-        atoms.calc = Gaussian()
-        params = {'chk': 'ethylene-SP.chk', 'nprocshared': '16',
-                  'output_type': 'T', 'b3lyp': None, 'epr-iii': None,
-                  'charge': 0, 'mult': 1}
-        atoms.calc.parameters = params
-        gaussian_io = ReadWriteGaussian(
-            params={'gaussian_input': 'ethylene-SP.com'})
+        try:
+            os.chdir(_TESTDATA_DIR)
+            out_folder = "test_gaussian"
+            os.mkdir(out_folder)
+            sname = out_folder
+            atoms = Atoms('C2H4', positions=_positions, cell = _cell, pbc=True)
+            muon = Atom('H', position=[1.00000000, 0.00000000,
+                                    0.66748000])
+            atoms = atoms + muon
+            atoms.calc = Gaussian()
+            params = {'chk': 'ethylene-SP.chk', 'nprocshared': '16',
+                    'output_type': 'T', 'b3lyp': None, 'epr-iii': None,
+                    'charge': 0, 'mult': 1}
+            atoms.calc.parameters = params
+            gaussian_io = ReadWriteGaussian(
+                params={'gaussian_input': 'ethylene-SP.com'})
 
-        atoms_copy = atoms.copy()
-        atoms_copy.calc = copy.copy(atoms.calc)
-        gaussian_io.write(atoms_copy, out_folder, sname)
+            atoms_copy = atoms.copy()
+            atoms_copy.calc = copy.copy(atoms.calc)
+            gaussian_io.write(atoms_copy, out_folder, sname)
 
-        # add muon properties to atoms to simulate what the write method
-        # should be doing:
+            # add muon properties to atoms to simulate what the write method
+            # should be doing:
 
-        masses = [None] * len(atoms.numbers)
-        masses[-1] = constants.m_mu_amu
-        atoms.set_masses(masses)
+            masses = [None] * len(atoms.numbers)
+            masses[-1] = constants.m_mu_amu
+            atoms.set_masses(masses)
 
-        NMagMs = [None] * len(atoms.numbers)
-        NMagMs[-1] = constants.mu_nmagm
-        atoms.new_array('gaussian_NMagM', np.array(NMagMs))
+            NMagMs = [None] * len(atoms.numbers)
+            NMagMs[-1] = constants.mu_nmagm
+            atoms.new_array('gaussian_NMagM', np.array(NMagMs))
 
-        # check if OG atoms should be modified if we don't save a copy
-        atoms_read = io.read(os.path.join(out_folder,'test_gaussian.com'), get_calculator=True)
+            # check if OG atoms should be modified if we don't save a copy
+            atoms_read = io.read(os.path.join(out_folder,'test_gaussian.com'), get_calculator=True)
 
-        # Checks properties of the atoms are written out correctly
-        assert np.all(atoms_read.numbers == atoms.numbers)
-        assert np.allclose(atoms_read.positions, atoms.positions, atol=1e-3)
-        assert np.all(atoms_read.pbc == atoms.pbc)
-        assert np.allclose(atoms_read.cell, atoms.cell)
+            # Checks properties of the atoms are written out correctly
+            assert np.all(atoms_read.numbers == atoms.numbers)
+            assert np.allclose(atoms_read.positions, atoms.positions, atol=1e-3)
+            assert np.all(atoms_read.pbc == atoms.pbc)
+            assert np.allclose(atoms_read.cell, atoms.cell)
 
-        # checks that the muon properties have been correctly added by the
-        # ReadWrite class:
+            # checks that the muon properties have been correctly added by the
+            # ReadWrite class:
 
-        assert np.allclose(atoms_read.get_masses(), atoms.get_masses())
-        assert (atoms_read.arrays['gaussian_NMagM']
-                == atoms.arrays['gaussian_NMagM']).all()
+            assert np.allclose(atoms_read.get_masses(), atoms.get_masses())
+            assert (atoms_read.arrays['gaussian_NMagM']
+                    == atoms.arrays['gaussian_NMagM']).all()
 
-        new_params = atoms_read.calc.parameters
-        matching_params = {k: new_params[k] for k in new_params
-                           if k in params and new_params[k] == params[k]}
+            new_params = atoms_read.calc.parameters
+            matching_params = {k: new_params[k] for k in new_params
+                            if k in params and new_params[k] == params[k]}
 
-        assert (len(params) == len(matching_params))
+            assert (len(params) == len(matching_params))
 
-        shutil.rmtree('test_gaussian')
+        finally:
+            shutil.rmtree('test_gaussian')
+
 
 
 if __name__ == "__main__":
